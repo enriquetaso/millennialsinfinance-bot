98170098ef83504cb9bfad735d77c64b568a37a6
Sofia Enriquez <lsofia.enriquez@gmail.com>
2023-02-17 19:03:34 +0000

    :tada: Finance bot init
    Main files

---
+++ README.md
@@ -0,0 +1,4 @@
+ You will find it at t.me/MillennialsInFinanceBot. 
+
+name: Millennials In Finance
+username: @MillennialsInFinanceBot
---
+++ financebot.py
@@ -0,0 +1,25 @@
+import os
+import logging
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN", "none")
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
